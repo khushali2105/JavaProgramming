8828a6c044f041a1c58db87931c6c9c022191832
khushali2105 <0521sapna@gmail.com>
2024-01-24 16:11:25 -0500

    some changes made in demo1 file

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,6 +2,7 @@ public class Demo1 {
     public static void main(String[]args){
         //comment added
         System.out.println("hello folks!!!!!!!!!");
+        System.out.println("hello girls!!!!!!!!!");
         System.out.println("How are you?");
     }
 }
