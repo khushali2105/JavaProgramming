ca006477d084395b6f52b74a8fc34fd1fffdc5d4
khushali2105 <0521sapna@gmail.com>
2024-01-24 13:15:58 -0500

    demo1 and demo 2 added

---
+++ src/demo1.java
@@ -0,0 +1,9 @@
+
+public class demo1 {
+//for learning git commands
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+        System.out.println("how are you?");
+    }
+}
