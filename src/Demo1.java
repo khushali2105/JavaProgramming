f8def54dcc62311e150c4d6aa4643ce311ee64e7
khushali2105 <35130562+khushali2105@users.noreply.github.com>
2024-01-24 16:09:10 -0500

    Update Demo1.java
    Changes made in demo.java file

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,7 +1,7 @@
 public class Demo1 {
     public static void main(String[]args){
         //comment added
-        System.out.println("hello folks!!!!!!!!!");
+        System.out.println("hello guys!!!!!!!!!");
         System.out.println("How are you?");
     }
 }
